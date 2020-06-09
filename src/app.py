1ebd882a03c5e77cc3e5b03c56d2b0b78a4aa2eb
Ayobeen <abdlazeezmumeen@gmail.com>
2020-06-09 16:09:48 +0100

    Chatbot with frontend integration

--- src/app.py
+++ src/app.py
@@ -1,2 +1,126 @@
-def run():
-    pass
+from flask import Flask, render_template, request
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow as tf
+import random
+import json
+import pickle
+
+app = Flask(__name__)
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w) for w in doc]
+
+
+    # one hot encoding
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+tf.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+ 
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for sw in s_words:
+        for i, w in enumerate(words):
+            if w == sw:
+                bag[i] = 1
+    return numpy.array(bag)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/get")
+def get_bot_response():
+    while True:
+        userText = request.args.get('msg')
+        if userText.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(userText, words)])[0]
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        if results[results_index] > 0.7:
+            for tg in data["intents"]:
+                if tg["tag"] == tag:
+                    responses = tg["responses"]
+            return random.choice(responses)
+        else:
+            return "Oh sorry! I didn't understand that. Kindly ask a different question."
+    
+
+if __name__ == "__main__":
+    app.run()
+
